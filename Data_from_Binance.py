8c6d99837f2ffd36c41513df92657739271a3a5b
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-05 21:51:23 +0200

    Update Data_from_Binance.py

--- Data_from_Binance.py
+++ Data_from_Binance.py
@@ -1,5 +1,5 @@
 # This is how I get and how I manage data from Binance
-# (I am using API and not the Websocket beacuse (personally) I don't need a continuous comunication with Binance)
+# I'm using API and not the Websocket beacuse (personally) I don't need a continuous comunication with Binance
 
 from binance import Client
 import pandas as pd
@@ -12,7 +12,7 @@ client = Client(api_key, api_secret)
 # Getting data:
 
 data = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "21 Feb, 2022", "20 Feb, 2023")
-# data = client.get_historical_klines("ETHBUSD", Client.KLINE_INTERVAL_1DAY, "21 Feb, 2022")
+# data = client.get_historical_klines("ETHBUSD", Client.KLINE_INTERVAL_1DAY, "21 Feb, 2022") # up to now
 # data = client.get_historical_klines("BTCETH", Client.KLINE_INTERVAL_1DAY, "1 day ago UTC") # up to now
 
 # Organizing data:
