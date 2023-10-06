241d146dcd52fe65763ac39d3388f554f4b29a34
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 19:48:29 +0200

    Update BOLLINGER_BANDS.py

--- INDICATORS/BOLLINGER_BANDS.py
+++ INDICATORS/BOLLINGER_BANDS.py
@@ -1,11 +1,46 @@
+# LOWER Bollinger Band
+
 def BBL(array, period, k):
+    """
+    Calculate the Lower Bollinger Band of given prices.
+    :param array: The list of the Close prices of the financial instrument.
+    :param period: The period to use for the BB calculation.
+    :param k: The standard deviation.
+    :return: The values of the Lower BB calculated based on prices, period and sd given.
+    """
     
-    BB1 = SMA(array, period) - k * array.rolling(period).std()
+    BBlow = array.rolling(period).mean() - k * array.rolling(period).std()
     
-    return BB1
+    return BBlow
+
+# MID Bollinger Band
+
+def BBM(array, period, k):
+    """
+    Calculate the Mid Bollinger Band (mean of the Upper and Lower BBs) of given prices.
+    :param array: The list of the Close prices of the financial instrument.
+    :param period: The period to use for the BB calculation.
+    :param k: The standard deviation.
+    :return: The values of the Mid BB calculated based on prices, period and sd given.
+    """
+    
+    BBup = array.rolling(period).mean() + k * array.rolling(period).std()
+    BBlow = array.rolling(period).mean() - k * array.rolling(period).std()
+    BBmid = (BBup + BBlow) / 2
+    
+    return BBmid
+
+# UPPER Bollinger Band
 
 def BBU(array, period, k):
+    """
+    Calculate the Upper Bollinger Band of given prices.
+    :param array: The list of the Close prices of the financial instrument.
+    :param period: The period to use for the BB calculation.
+    :param k: The standard deviation.
+    :return: The values of the Upper BB calculated based on prices, period and sd given.
+    """
     
-    BB2 = SMA(array, period) + k * array.rolling(period).std()
+    BBup = array.rolling(period).mean() + k * array.rolling(period).std()
     
-    return BB2
+    return BBup
