9359584771d7421fc320f7963349a24682a4612d
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 11:36:30 +0200

    Update RSI.py

--- INDICATORS/RSI.py
+++ INDICATORS/RSI.py
@@ -2,7 +2,7 @@ def RSI(array, period):
 
   var = array.diff()
   gain = var.where(var > 0, 0)
-  loss = -var.where(var < 0, 0)
+  loss = - var.where(var < 0, 0)
   avg_gain = gain.rolling(period).mean()
   avg_loss = loss.rolling(period).mean()
   rs = avg_gain / avg_loss
