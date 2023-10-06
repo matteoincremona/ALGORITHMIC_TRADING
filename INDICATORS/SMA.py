3ed991dd8fd8bc459ee00d2f76b3596daa52cc6a
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 19:28:32 +0200

    Update SMA.py

--- INDICATORS/SMA.py
+++ INDICATORS/SMA.py
@@ -4,7 +4,6 @@ def SMA(array, period):
     :param array: The list of the Close prices of the financial instrument.
     :param period: The period to use for the SMA calculation.
     :return: The values of the SMA calculated based on the prices and the period given.
-    
     """
     
     return array.rolling(period).mean()
