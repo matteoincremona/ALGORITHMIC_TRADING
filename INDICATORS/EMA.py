9b28298dc8269667de55030c76fcb26408d057ca
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 11:48:34 +0200

    Update EMA.py

--- INDICATORS/EMA.py
+++ INDICATORS/EMA.py
@@ -1,5 +1,5 @@
 def EMA(array, period):
     
     res = array.ewm(span = period, adjust = False).mean()
-    
+    # alpha = 1/n --> see on tradingview which if the values are correct
     return res
