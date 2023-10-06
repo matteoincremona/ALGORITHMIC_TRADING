1464f802a6868b65015eaecfb7a4542350a37967
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 11:44:21 +0200

    Create MACD.py

---
+++ INDICATORS/MACD.py
@@ -0,0 +1,7 @@
+#def MACD(array, slow_n, fast_n):
+#
+#  slow = array.ewm(span = slow_n, adjust = False).mean()
+#  fast = array.ewm(span = fast_n, adjust = False).mean()
+#  macd = slow - fast
+#  
+#  return macd
