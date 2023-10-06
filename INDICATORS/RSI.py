31c967264ced9244aed64b937ccf262c250f1050
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 18:59:42 +0200

    Update RSI.py

--- INDICATORS/RSI.py
+++ INDICATORS/RSI.py
@@ -1,11 +1,19 @@
-#def RSI(array, period):
-#
-#  var = array.diff()
-#  gain = var.where(var > 0, 0)
-#  loss = - var.where(var < 0, 0)
-#  avg_gain = gain.rolling(period).mean()
-#  avg_loss = loss.rolling(period).mean()
-#  rs = avg_gain / avg_loss
-#  rsi = 100 - (100 / (1 + rs))
-#  
-#  return rsi
+def RSI(array, period):
+  """
+  Calculate the Relative Strenght Index (RSI) values.
+
+  :param array: The list of the Close prices of the financial instrument.
+  :param period: The period to use for the RSI (default = 14).
+  :return: The values of the RSI calculated based on the prices and the period given.
+  
+  """
+  
+  var = array.diff(), ndigits = 5
+  gain = var.where(var > 0, 0)
+  loss = - var.where(var < 0, 0)
+  avg_gain = gain.rolling(period).mean()
+  avg_loss = loss.rolling(period).mean()
+  rs = avg_gain / avg_loss
+  rsi = 100 - (100 / (1 + rs))
+  
+  return rsi
