45d25d19eadcc812430a8dd44f9bfec1031766f4
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-06 19:18:32 +0200

    Update EMA.py

--- INDICATORS/EMA.py
+++ INDICATORS/EMA.py
@@ -1,5 +1,11 @@
 def EMA(array, period):
+    """
+    Calculate the Esponential Moving Average (EMA), also called Esponential Weighted Moving Average (EWMA).
+    :param array: The list of the Close prices of the financial instrument.
+    :param period: The period to use for the EMA calculation.
+    :return: The values of the EMA calculated based on the prices and the period given.
     
+    """
     res = array.ewm(span = period, adjust = False).mean()
-    # alpha = 1/n --> see on tradingview which if the values are correct
+    
     return res
