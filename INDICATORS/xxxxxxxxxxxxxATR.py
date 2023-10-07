d83cc3509b6066c30bf6c8fc02088da7d685d0fe
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-07 20:47:30 +0200

    Update xxxxxxxxxxxxxATR.py

--- INDICATORS/xxxxxxxxxxxxxATR.py
+++ INDICATORS/xxxxxxxxxxxxxATR.py
@@ -1,10 +1,17 @@
 def ATR(High, Low, Close, n):
-
+    """
+    Calculate the Average True Range (ATR).
+    :param High: The list of the High prices of the financial instrument.
+    :param Low: The list of the Low prices of the financial instrument.
+    :param Close: The list of the Close prices of the financial instrument.
+    :param n: The period to use for the ATR calculation.
+    :return: The values of the ATR calculated based on the prices and the period given.
+    """
     tr0 = abs(High - Low)
     tr1 = abs(High - Close.shift())
     tr2 = abs(Low - Close.shift())
-    tr = [tr0, tr1, tr2].max(axis = 1)
-    ema = values.ewm(span = n, adjust = False).mean()
-    atr = ema(tr, n)
+    db = pd.DataFrame({"tr0": tr0, "tr1": tr1, "tr2": tr2})
+    tr = db.max(axis = 1)
+    atr = tr.ewm(span = n, adjust = False).mean()
   
     return atr
