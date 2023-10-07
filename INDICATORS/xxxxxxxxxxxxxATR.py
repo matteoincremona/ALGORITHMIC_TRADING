281cc9d273ad39288ae7d63ad64c413351088b49
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-07 03:34:11 +0200

    Update xxxxxxxxxxxxxATR.py

--- INDICATORS/xxxxxxxxxxxxxATR.py
+++ INDICATORS/xxxxxxxxxxxxxATR.py
@@ -1,10 +1,10 @@
-#def ATR(High, Low, Close, n):
-#
-#    tr0 = abs(High - Low)
-#    tr1 = abs(High - Close.shift())
-#    tr2 = abs(Low - Close.shift())
-#    tr = [tr0, tr1, tr2].max(axis = 1)
-#    ema = values.ewm(span = n, adjust = False).mean()
-#    atr = ema(tr, n)
-#  
-#    return atr
+def ATR(High, Low, Close, n):
+
+    tr0 = abs(High - Low)
+    tr1 = abs(High - Close.shift())
+    tr2 = abs(Low - Close.shift())
+    tr = [tr0, tr1, tr2].max(axis = 1)
+    ema = values.ewm(span = n, adjust = False).mean()
+    atr = ema(tr, n)
+  
+    return atr
