e6beed975f0ded84c121535908528d61de257d0e
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-09 17:24:22 +0200

    Update SMA.py

--- INDICATORS/SMA.py
+++ INDICATORS/SMA.py
@@ -3,11 +3,11 @@ def SMA(array, period):
     
     Calculate the Single Moving Average (SMA) of given prices.
 
-    Args
+    Args:
     array (pandas.Series): The list of the Close prices of the financial instrument.
     period (int): The period to use for the SMA calculation.
     
-    Return: The values of the SMA calculated based on the prices and the period given.
+    Return (pandas.Series): The values of the SMA calculated based on the prices and the period given.
     """
     
     return array.rolling(period).mean()
