0dde275ecd8b104c7ec3e0944a16af07e0c02070
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-09 17:27:37 +0200

    Update MACD.py

--- INDICATORS/MACD.py
+++ INDICATORS/MACD.py
@@ -1,10 +1,14 @@
 def MACD(array, slow_n, fast_n):
-  """
+  """Moving Average Convergence Divergence
+  
   Calculate the Moving Average Convergence Divergence (MACD).
-  :param array: The list of the Close prices of the financial instrument.
-  :param slow_n: The value of the period for the calculation of the slow moving average (default = 26).
-  :param fast_n: The value of the period for the calculation of the fast moving average (default = 12).
-  :return: The values of the MACD calculated based on the prices and periods given.
+
+  Args:
+  array (pandas.Series): The list of the Close prices of the financial instrument.
+  slow_n (int): The value of the period for the calculation of the slow moving average (default = 26).
+  fast_n (int): The value of the period for the calculation of the fast moving average (default = 12).
+  
+  Return (pandas.Series): The values of the MACD calculated based on the prices and periods given.
   """
   slow = array.ewm(span = slow_n, adjust = False).mean()
   fast = array.ewm(span = fast_n, adjust = False).mean()
@@ -15,11 +19,15 @@ def MACD(array, slow_n, fast_n):
 # We can also calculate a signal for the MACD, that is a moving average based on the MACD values.
 
 def MACDsignal(macd_values, period):
-  """
+  """Moving Average Convergence Divergence Signal
+
   Calculate a moving average based on the MACD values and a given period.
-  :param macd_values: The values of the MACD of the financial instrument.
-  :param period: The value of the period for the calculation of the moving average (default = 9)
-  :return: The values of the moving average based on the MACD values and the given period.
+
+  Args:
+  macd_values (pandas.Series): The values of the MACD of the financial instrument. (see function called "MACD").
+  period (int): The value of the period for the calculation of the moving average (default = 9).
+  
+  Return (pandas.Series): The values of the moving average based on the MACD values and the given period.
   """
   sig = macd_values.ewm(span = period, adjust = False).mean()
 
