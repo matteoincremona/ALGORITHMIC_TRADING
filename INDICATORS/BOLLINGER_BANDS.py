f51b80ef98fda924a1d27702ee7365b957356a5b
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-09 17:34:12 +0200

    Update BOLLINGER_BANDS.py

--- INDICATORS/BOLLINGER_BANDS.py
+++ INDICATORS/BOLLINGER_BANDS.py
@@ -1,12 +1,16 @@
 # LOWER Bollinger Band
 
 def BBL(array, period, k):
-    """
+    """Lower Bollinger Band
+    
     Calculate the Lower Bollinger Band of given prices.
-    :param array: The list of the Close prices of the financial instrument.
-    :param period: The period to use for the BB calculation.
-    :param k: The standard deviation.
-    :return: The values of the Lower BB calculated based on prices, period and sd given.
+
+    Args:
+    array (pandas.Series): The list of the Close prices of the financial instrument.
+    period (int): The period to use for the BB calculation (default = 20).
+    k (int): The standard deviation (default = 2).
+    
+    Return (pandas.Series): The values of the Lower BB calculated based on prices, period and sd given.
     """
     
     BBlow = array.rolling(period).mean() - k * array.rolling(period).std()
@@ -16,12 +20,16 @@ def BBL(array, period, k):
 # MID Bollinger Band
 
 def BBM(array, period, k):
-    """
+    """Mid Bollinger Band
+    
     Calculate the Mid Bollinger Band (mean of the Upper and Lower BBs) of given prices.
-    :param array: The list of the Close prices of the financial instrument.
-    :param period: The period to use for the BB calculation.
-    :param k: The standard deviation.
-    :return: The values of the Mid BB calculated based on prices, period and sd given.
+
+    Args:
+    array (pandas.Series): The list of the Close prices of the financial instrument.
+    period (int): The period to use for the BB calculation (default = 20).
+    k (int): The standard deviation (default = 2).
+    
+    Return (pandas.Series): The values of the Mid BB calculated based on prices, period and sd given.
     """
     
     BBup = array.rolling(period).mean() + k * array.rolling(period).std()
@@ -33,12 +41,16 @@ def BBM(array, period, k):
 # UPPER Bollinger Band
 
 def BBU(array, period, k):
-    """
+    """Upper Bollinger Band
+    
     Calculate the Upper Bollinger Band of given prices.
-    :param array: The list of the Close prices of the financial instrument.
-    :param period: The period to use for the BB calculation.
-    :param k: The standard deviation.
-    :return: The values of the Upper BB calculated based on prices, period and sd given.
+
+    Args:
+    array (pandas.Series): The list of the Close prices of the financial instrument.
+    period (int): The period to use for the BB calculation (default = 20).
+    k (int): The standard deviation (default = 2).
+    
+    Return (pandas.Series): The values of the Upper BB calculated based on prices, period and sd given.
     """
     
     BBup = array.rolling(period).mean() + k * array.rolling(period).std()
