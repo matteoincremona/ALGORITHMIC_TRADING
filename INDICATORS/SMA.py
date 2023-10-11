6b705419e4b09ffcf62612813c3463c3d0afe014
Matteo Incremona <126263142+matteoincremona@users.noreply.github.com>
2023-10-11 12:02:05 +0200

    Update SMA.py

--- INDICATORS/SMA.py
+++ INDICATORS/SMA.py
@@ -8,6 +8,8 @@ def SMA(array, period):
     - period (int): The period to use for the SMA calculation.
     
     It returns (pandas.Series) the values of the SMA calculated based on the prices and the period given.
+
+    Source: https://www.investopedia.com/terms/m/movingaverage.asp
     """
     
     return array.rolling(period).mean()
